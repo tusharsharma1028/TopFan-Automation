7568824da734e09f2d0506224f37c8b4f18b5e85
tusharsharma1028 <tushar.sharma@unthinkable.co>
2026-04-20 18:46:11 +0530

    feat: add src/components/index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from './common/Button/Button';
+export { default as Input } from './common/Input/Input';
+export { default as Loader } from './common/Loader/Loader';
+export { default as Header } from './layout/Header/Header';
+export { default as Footer } from './layout/Footer/Footer';
+export { default as Sidebar } from './layout/Sidebar/Sidebar';
+export { default as Layout } from './layout/Layout';
